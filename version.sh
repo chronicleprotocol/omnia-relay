9f10fd639f91a8655fa8370167498e987c8735c5
Paweł Zaremba <pawzar@gmail.com>
2023-06-30 15:11:27 +0200

    Remove Cast

--- version.sh
+++ version.sh
@@ -3,16 +3,19 @@ set -xeuo pipefail
 # semver: 3.4.0
 
 # bump to next major
-semver bump major  "$(head -1 ./version | tr -d '\n')" | tee ./version
+_VERSION=$(semver bump major  "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
 # bump to next minor
-semver bump minor  "$(head -1 ./version | tr -d '\n')" | tee ./version
+_VERSION=$(semver bump minor  "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
 # bump to next patch
-semver bump patch  "$(head -1 ./version | tr -d '\n')" | tee ./version
+_VERSION=$(semver bump patch  "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
 # bump PRE version
-semver bump prerel "$(head -1 ./version | tr -d '\n')" | tee ./version
+_VERSION=$(semver bump prerel "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
+
+# bump PRE version
+_VERSION=$(semver bump prerel dev.0 "$(head -1 ./version | tr -d '\n')") && tee <<<"$_VERSION" ./version
 
 git commit -m "Bump version to 'v$(head -1 ./version | tr -d '\n')'" ./version \
 && git tag "v$(head -1 ./version | tr -d '\n')" \
